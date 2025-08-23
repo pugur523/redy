66b16d6ac288b558f96f1917723b52ea965f864d
pugur <pugurmc@gmail.com>
2025-08-23 10:33:14 +0900

    bench: add tokenize bulk benchmark

--- src/frontend/processor/lexer/lexer_bench.cc
+++ src/frontend/processor/lexer/lexer_bench.cc
@@ -53,6 +53,25 @@ void lexer_loop(benchmark::State& state) {
 }
 BENCHMARK(lexer_loop);
 
+void lexer_tokenize_bulk(benchmark::State& state) {
+  std::u8string code =
+      u8"x := 42 while x < 100 { x = x + 1 } for i: 0..<100 { ++i } ret 0";
+  std::size_t code_size = code.size();
+
+  unicode::Utf8FileManager manager;
+  unicode::Utf8FileId id = manager.register_virtual_file(std::move(code));
+
+  Lexer lexer;
+  auto init_result = lexer.init(&manager, id);
+  for (auto _ : state) {
+    const auto result = lexer.tokenize();
+    benchmark::DoNotOptimize(std::move(result).unwrap().size());
+    lexer.reset();
+  }
+  state.SetBytesProcessed(code_size * state.iterations());
+}
+BENCHMARK(lexer_tokenize_bulk);
+
 }  // namespace
 
 }  // namespace lexer
